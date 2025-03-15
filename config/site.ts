52c1a7186da128a2ecfa9429762ea30e1ed3e25f
jDesigns <jay.designs716@gmail.com>
2025-03-15 11:57:50 -0400

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "RichsCozyCorner",
+  url: "http://localhost:3000",
+  ogImage: "https://ui.shadcn.com/og.jpg",
+  description:
+    "Welcome to RichsCozyCorner – a chill and cozy Twitch stream where you can relax, vibe, and enjoy great gameplay, comfy chats, and a welcoming community. Tune in for laid-back streams and good company!",
+  links: {
+    twitch: "https://www.twitch.tv/richscozycorner",
+    youtube: "https://www.youtube.com/@RichsCozyCorner",
+    instagram: "https://www.instagram.com/richscozycorner",
+    tiktok: "https://www.tiktok.com/@richscozycorner",
+  },
+  devLinks: {
+    twitter: "https://x.com/Jay716_",
+    github: "https://github.com/JDesigns716",
+    projectPath: "https://github.com/JDesigns716",
+  },
+};
